787b9803e5d92939ceb3300fd67684130df4e020
Diego Puente <21991012+DiegoPGs@users.noreply.github.com>
2020-05-01 12:34:42 -0500

    Actualización de script principal
    Optimización de velocidad
    Ahora el programa no crashea al caer en una paǵina que ya no sea gratuita
    Legibilidad de código

--- el_scripto.py
+++ el_scripto.py
@@ -1,37 +1,46 @@
-import PyPDF2
-import requests
-from bs4 import BeautifulSoup
+#modulos de python
 import os
+import re
+import requests
+#modulos de terceros
+import PyPDF2
+from bs4 import BeautifulSoup
 
-PDFFile = open('/home/diego/Downloads/Telegram Desktop/Springer Ebooks.pdf','rb')
+def Buscar_URLs(s):
+	pattern = "http:\/\/link\.springer\.com\/openurl\?genre\=book\&isbn\=\d+-\d+-\d+\d+-\d+-\d+"
+	urls = re.findall(pattern, s)
+	return urls
 
-PDF = PyPDF2.PdfFileReader(PDFFile)
-pages = PDF.getNumPages()
-key = '/Annots'
-uri = '/URI'
-ank = '/A'
+def main():
+	ruta_origen = open('/home/diego/Downloads/Telegram Desktop/Springer Ebooks.pdf','rb')
 
-for page in range(pages):
-	pageSliced = PDF.getPage(page)
-	pageObject = pageSliced.getObject()
-	if key in pageObject.keys():
-		ann = pageObject[key]
-		for a in ann:
-			u = a.getObject()
-			if uri in u[ank].keys():
-				url = u[ank][uri]
-				print(url)
-				r = requests.get(url)
-				contenido = r.content
-				la_sopita = BeautifulSoup(contenido, 'html.parser')
-				titulo = la_sopita.select('h1')[0].text.strip()
-				auths = la_sopita.findAll("span", {"class" : "authors__name"})
+	PDF = PyPDF2.PdfFileReader(ruta_origen)
+	pages = PDF.getNumPages()
+
+	for page in range(pages):
+		#texto de pag
+		pageSliced = PDF.getPage(page)
+		pageObject = pageSliced.getObject()
+		text = pageObject.extractText()
+
+		#urls en pag
+		urls = Buscar_URLs(text)
+
+		for url in urls:
+			#peticion
+			r = requests.get(url)
+			la_sopita = BeautifulSoup(r.content, 'html.parser')
+
+			#contenido
+			titulo = la_sopita.select('h1')[0].text.strip()
+			auths = la_sopita.findAll("span", {"class" : "authors__name"})
+
+			#revisar descarga gratuita
+			if la_sopita.find("a", {"class" : "test-bookpdf-link"}):
 				download_pdf = "https://link.springer.com" + la_sopita.find("a", {"class" : "test-bookpdf-link"})['href']
-				autor = ""
-				
-				print(download_pdf)
-				#exit()
 
+				#nombre de autor(es)
+				autor = ""
 				for i in range(len(auths)):
 					a = auths[i]
 					if len(auths) == 1:
@@ -40,9 +49,16 @@ for page in range(pages):
 						autor += a.text.strip() + ", "
 						if i == (len(auths) - 1):
 							autor += a.text.strip()
-
-				file_name = titulo + ": " + autor + ".pdf"
-				print(file_name)
 				
+				file_name = titulo + ": " + autor + ".pdf"
+
+				#mostrar info al usuario
+				print(file_name)
+				print(url)
+
+				#descargar pdf
 				myfile = requests.get(download_pdf, allow_redirects=True)
 				open(file_name, 'wb').write(myfile.content)
+
+if __name__ == '__main__':
+	main()
